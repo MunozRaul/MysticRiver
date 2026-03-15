e7e1c708c810a2e5dc785420ade614731ed0ef97
menaruben <ruben@mena.ch>
2026-03-15 09:16:02 +0100

    code of conduct signature, role, rename develop -> staging

---
+++ src/MysticRiver.TestFramework/ITestSetup.cs
@@ -0,0 +1,14 @@
+﻿using DotNet.Testcontainers.Containers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MysticRiver.TestFramework;
+
+public interface ITestSetup : IAsyncDisposable
+{
+    public ValueTask ConfigureServicesAsync(
+        IServiceCollection services, 
+        IConfigurationManager configuration, 
+        IList<IContainer> containers
+    );
+}
---
+++ src/MysticRiver.TestFramework/TestContainerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace MysticRiver.TestFramework;
+
+public static class TestContainerExtensions
+{
+    private const string PostgreSqlImage = "postgres:18.3";
+    
+    public static async Task<PostgreSqlContainer> UsePostgreSqlAsync(
+        this IList<IContainer> containers,
+        Action<PostgreSqlBuilder> configureSqlServerBuilder)
+    {
+        var builder = new PostgreSqlBuilder(PostgreSqlImage);
+        configureSqlServerBuilder(builder);
+        
+        var container = builder.Build();
+        containers.Add(container);
+        await container.StartAsync().ConfigureAwait(false);
+        return container;
+    }
+}
