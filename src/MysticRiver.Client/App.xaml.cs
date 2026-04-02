63c7eda3c1e4454802de73264892f91d934c2825
MunozRaul <149008295+MunozRaul@users.noreply.github.com>
2026-04-02 23:24:33 +0200

    feat: auto-update client on new releases (#44)
    * feat: auto-update client on new releases
    
    * PR comments
    
    * setup DI for client
    
    * remove leading 'v' letter

--- src/MysticRiver.Client/App.xaml.cs
+++ src/MysticRiver.Client/App.xaml.cs
@@ -1,14 +1,42 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace MysticRiver.Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private readonly IHost _host;
 
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<UpdateService>();
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            await _host.StartAsync();
+
+            var updater = _host.Services.GetRequiredService<UpdateService>();
+            updater.CheckForUpdates();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
+        }
+    }
 }
