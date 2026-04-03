30677cac346d92100c965d9f6dcd28f157ec03d6
Rubén Mena <ruben@mena.ch>
2026-04-03 13:34:05 +0000

    feat: add programming guidelines (#53)
    * feat: added editorconfig and Build props
    
    * formatting + editorconfig changes for private readonly fields and usings, casts, unused paramters

--- src/MysticRiver.Client/App.xaml.cs
+++ src/MysticRiver.Client/App.xaml.cs
@@ -1,46 +1,40 @@
 using System.Windows;
 
+using System.Windows;
+
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-
-using MysticRiver.Client.Options;
-using MysticRiver.Client.Services;
 
 namespace MysticRiver.Client;
 
 public partial class App : Application {
     private readonly IHost _host;
-    public static IServiceProvider Services { get; private set; } = null!;
 
     public App() {
         _host = Host.CreateDefaultBuilder()
-            .ConfigureServices((context, services) => {
-                services.AddOptions<ClientOptions>()
-                    .Bind(context.Configuration.GetSection(ClientOptions.SectionName));
-                services.AddSingleton(provider => provider.GetRequiredService<IOptions<ClientOptions>>().Value);
-
-                services.AddHttpClient<BattleApiClient>((provider, client) => {
-                    var clientOptions = provider.GetRequiredService<ClientOptions>();
-                    client.BaseAddress = ResolveApiBaseUri(clientOptions);
-                });
-                services.AddSingleton<BattleRealtimeClient>();
+            .ConfigureServices(services => {
                 services.AddSingleton<UpdateService>();
                 services.AddSingleton<MainWindow>();
             })
             .Build();
-
-        Services = _host.Services;
     }
 
     protected override async void OnStartup(StartupEventArgs e) {
         base.OnStartup(e);
+    protected override async void OnStartup(StartupEventArgs e) {
+        base.OnStartup(e);
 
+        await _host.StartAsync();
         await _host.StartAsync();
 
         var updater = _host.Services.GetRequiredService<UpdateService>();
         updater.CheckForUpdates();
+        var updater = _host.Services.GetRequiredService<UpdateService>();
+        updater.CheckForUpdates();
 
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
         var mainWindow = _host.Services.GetRequiredService<MainWindow>();
         mainWindow.Show();
     }
@@ -49,15 +43,5 @@ public partial class App : Application {
         await _host.StopAsync();
         _host.Dispose();
         base.OnExit(e);
-    }
-
-    private static Uri ResolveApiBaseUri(ClientOptions clientOptions) {
-        ArgumentNullException.ThrowIfNull(clientOptions);
-
-        if (!Uri.TryCreate(clientOptions.ApiBaseUrl, UriKind.Absolute, out var uri)) {
-            throw new InvalidOperationException($"Invalid configuration value for {ClientOptions.SectionName}:ApiBaseUrl.");
-        }
-
-        return uri;
     }
 }
